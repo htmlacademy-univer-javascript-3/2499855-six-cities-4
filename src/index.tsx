2d3ab9a66d3f9ffe510f6a97db98c427c677c25f
macadamiaboy <elknumber1@yandex.com>
2024-03-03 12:53:49 +0300

    Location of app.tsx is changed

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/app/app';
+import App from './app';
+
+const Preset = {
+  placesCount: 16,
+} as const;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -8,6 +12,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App placesCount={250} />
+    <App placesCount={Preset.placesCount} />
   </React.StrictMode>
 );
